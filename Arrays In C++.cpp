8cebb2381742ff61ca10a3ee76f5ebf861fe1fdc
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-08-05 19:15:52 +0530

    Arrays In C++.cpp

---
+++ Arrays In C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int marks[]={50,60,70,80,90};
+    for(int i=0;i<5;i++){
+        cout<<marks[i]<<"\n";
+    }
+    return 0;
+}
+/*
+Output :
+50
+60
+70
+80
+90
+*/
