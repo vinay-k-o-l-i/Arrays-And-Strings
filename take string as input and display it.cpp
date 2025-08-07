29f13dc9aa2637ed802bf0de3b6b038de1166044
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-08-08 00:07:58 +0530

    Create take string as input and display it.cpp

---
+++ take string as input and display it.cpp
@@ -0,0 +1,21 @@
+//take string as input and display it 
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Enter the string \n";
+    cin>>str;
+    cout<<"\nThe string is : "<<str;
+
+}   
+
+/* output for this code 
+
+Enter the string 
+vinay
+
+The string is : vinay
+
+*/
