db0714e6ce55b0f5cfb0a0e8d402d4935eb87905
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-08-08 00:04:56 +0530

    Create concatenation of string.cpp

---
+++ concatenation of string.cpp
@@ -0,0 +1,29 @@
+// concatenation of string
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1;
+    string str2;
+    cout<<"Enter the string 1 : \n";
+    cin>>str1;
+    cout<<"\nEnter the string 2 : \n";
+    cin>>str2;
+    cout<<"\nConcatenating both strings\n";
+    cout<<str1+" "+str2;
+
+}
+
+/* output for this code
+
+Enter the string 1 : 
+vinay
+
+Enter the string 2 : 
+koli
+
+Concatenating both strings
+vinaykoli
+
+*/
